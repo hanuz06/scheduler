45d612603c30f393c877f82c34e9c80c61ac10b1
Andrey Li <lee1975uz@gmail.com>
2019-11-28 18:50:47 -0700

    add spot increase/decrease functions

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -47,8 +47,6 @@ export default function useApplicationData() {
     interviewers:{}    
   })
 
-  console.log('DAYSSSSSSSSS',state.days )
-
   useEffect(() =>{ 
     Promise.all([
       Promise.resolve(
@@ -80,6 +78,19 @@ export default function useApplicationData() {
   // const setDay = day => setState({...state, day});
   const setDay = day => dispatch({type: SET_DAY, day});
 
+   //finds a day by appointment id
+   const dayByAppId= id => {
+    let dayByAppointmenID={};
+    state.days.forEach(item => {
+      item.appointments.forEach(appointmentID=>{
+        if(id===appointmentID){
+          dayByAppointmenID={...item};
+        }}
+      )
+    });
+    return dayByAppointmenID;
+  }
+
   function bookInterview(id, interview) {     
     
     const appointment = {
@@ -90,29 +101,11 @@ export default function useApplicationData() {
     const appointments = {
       ...state.appointments,
       [id]: appointment
-    };   
-
-
-    
-    //finds a day by appointment id
-    const dayByAppId= id => {
-      let dayByAppointmenID={};
-      state.days.forEach(item => {
-        item.appointments.forEach(appointmentID=>{
-          if(id===appointmentID){
-            dayByAppointmenID={...item};
-          }}
-        )
-      });
-      return dayByAppointmenID;
-    }
+    };     
 
     const daySpot = dayByAppId(id)
-   
-
     
-    const spotIncrease = (daySpot) =>{
-      console.log('dayByAppointmenID.id ', daySpot.id)
+    const spotIncrease = (daySpot) =>{      
       const m = state.days.map((item, index)=>{
         if (index !== daySpot.id-1){
           return item;
@@ -123,22 +116,9 @@ export default function useApplicationData() {
         }                 
       }       
       )
-
-      return m;
-  
+      return m;  
     }
     const days = spotIncrease(daySpot)
-
-  
- 
-
-    // const updatedDays = {
-    //   ...state.days,      
-    //   spotIncrease
-    // };   
-
-
-
 
     return axios.put(`/api/appointments/${id}`, {interview})
         .then(res=>
@@ -148,7 +128,6 @@ export default function useApplicationData() {
         // })
         dispatch({type: SET_INTERVIEW, appointments}),
         dispatch({type: SET_SPOT, days: days})
-
         )        
   } 
 
@@ -162,6 +141,23 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
+
+    const daySpot = dayByAppId(id)
+
+    const spotDecrease = (daySpot) =>{      
+      const m = state.days.map((item, index)=>{
+        if (index !== daySpot.id-1){
+          return item;
+        } 
+        return {
+          ...daySpot,
+          spots:item.spots + 1
+        }                 
+      }       
+      )
+      return m;  
+    }
+    const days = spotDecrease(daySpot)
    
     return axios.delete(`/api/appointments/${id}`)
         .then(res=>
@@ -169,7 +165,8 @@ export default function useApplicationData() {
         //   ...state,
         //   appointments
         // }))
-          dispatch({type: SET_INTERVIEW, appointments})
+          dispatch({type: SET_INTERVIEW, appointments}),
+          dispatch({type: SET_SPOT, days: days})
         )
     }
 
