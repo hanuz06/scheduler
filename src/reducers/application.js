abea61bb8100cba4a4d0cc5d4f037384680f7c4d
Andrey Li <lee1975uz@gmail.com>
2019-12-04 23:26:21 -0700

    commented out some codes in application.js to test circlei function

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -5,25 +5,25 @@ const SET_SPOT = "SET_SPOT";
 
   export default function reducer(state, action) {
     switch (action.type) {
-      case SET_DAY:
-        return { ...state, 
-        day: action.day }
-      case SET_APPLICATION_DATA:
-        return { ...state, 
-          days: action.days,
-          appointments: action.appointments,
-          interviewers: action.interviewers }
-      case SET_INTERVIEW: {
-        return { ...state, 
-          appointments: action.appointments }
-      }      
-      case SET_SPOT: {        
-        return { ...state, 
-          days: action.days }
-      }
-      default:
-        throw new Error(
-          `Tried to reduce with unsupported action type: ${action.type}`
-        );
+      // case SET_DAY:
+      //   return { ...state, 
+      //   day: action.day }
+      // case SET_APPLICATION_DATA:
+      //   return { ...state, 
+      //     days: action.days,
+      //     appointments: action.appointments,
+      //     interviewers: action.interviewers }
+      // case SET_INTERVIEW: {
+      //   return { ...state, 
+      //     appointments: action.appointments }
+      // }      
+      // case SET_SPOT: {        
+      //   return { ...state, 
+      //     days: action.days }
+      // }
+      // default:
+      //   throw new Error(
+      //     `Tried to reduce with unsupported action type: ${action.type}`
+      //   );
     }
   }
