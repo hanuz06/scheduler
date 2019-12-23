0136157c5cc7e0e259de8b5ade1b5d614c8ed2f9
Andrey Li <lee1975uz@gmail.com>
2019-12-22 19:50:40 -0700

    remove reducer function in useApplicationData

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -8,37 +8,7 @@ import reducer, {
 } from "reducers/application";
 import { statements } from '@babel/template';
 
-export default function useApplicationData() {  
-
-  // const SET_DAY = "SET_DAY";
-  // const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-  // const SET_INTERVIEW = "SET_INTERVIEW";
-  // const SET_SPOT = "SET_SPOT";
-
-  // function reducer(state, action) {
-  //   switch (action.type) {
-  //     case SET_DAY:
-  //       return { ...state, 
-  //       day: action.day }
-  //     case SET_APPLICATION_DATA:
-  //       return { ...state, 
-  //         days: action.days,
-  //         appointments: action.appointments,
-  //         interviewers: action.interviewers }
-  //     case SET_INTERVIEW: {
-  //       return { ...state, 
-  //         appointments: action.appointments }
-  //     }      
-  //     case SET_SPOT: {        
-  //       return { ...state, 
-  //         days: action.days }
-  //     }
-  //     default:
-  //       throw new Error(
-  //         `Tried to reduce with unsupported action type: ${action.type}`
-  //       );
-  //   }
-  // }
+export default function useApplicationData() {    
 
   const [state, dispatch]=useReducer(reducer, {
     day: "Monday",
